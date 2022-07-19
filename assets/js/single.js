958df24a805ce8adea13869f6a065d6e5e6a2486
dopecello <williamlocke.cello@gmail.com>
2022-07-18 21:56:41 -0400

    dynamic repo display and error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,18 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else {
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -16,14 +29,12 @@ var getRepoIssues = function (repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 
 
 };
-
-getRepoIssues("angular/angular");
 
 var displayIssues = function (issues) {
     if (issues.length === 0) {
@@ -72,3 +83,5 @@ var displayWarning = function (repo) {
     // append to warning container
     limitWarningEl.appendChild(linkEl);
 };
+
+getRepoName();
